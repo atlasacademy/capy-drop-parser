0273ae91952a2deefdff7482dfe0a5b605ba2ed2
squaresmile <squaresmile@protonmail.com>
2019-02-09 13:46:49 -0500

    Scroll bar got moved

--- fgo_mat_counter.py
+++ fgo_mat_counter.py
@@ -250,7 +250,7 @@ def get_qp(image):
 
 
 def get_scroll_bar_start_height(image):
-    gray_image = cv2.cvtColor(image[98:98 + 330, 920:920 + 27], cv2.COLOR_BGR2GRAY)
+    gray_image = cv2.cvtColor(image[98:98 + 330, 939:939 + 27], cv2.COLOR_BGR2GRAY)
     _, binary = cv2.threshold(gray_image, 225, 255, cv2.THRESH_BINARY)
     if LABEL: cv2.imwrite('scroll_bar_binary.png', binary)
     _, template = cv2.threshold(cv2.imread(os.path.join(REFFOLDER, 'scroll_bar_upper.png'), cv2.IMREAD_GRAYSCALE), 225, 255, cv2.THRESH_BINARY)
